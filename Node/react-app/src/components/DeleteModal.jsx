2d5b289efdaa2ef64ef83ca3a6a00aa536bde566
Omar Sayed <omarkhalil117@gmail.com>
2024-08-20 11:19:41 +0300

    add modals component to dashboard

--- Node/react-app/src/components/DeleteModal.jsx
+++ Node/react-app/src/components/DeleteModal.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 
-const DeleteModal = ({ onClose, onDelete }) => {
+const DeleteModal = ({ currentBook }) => {
+    
+  const deleteBook = () => {
+    axios.delete(`http://localhost:8080/api/books/${currentBook._id}`)
+    .then((res) => { 
+      console.log(res.data); 
+      alert(res.data.message);
+    } )
+    .catch( (err)=> console.log(err))
+  };
+
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <h4>Confirm Deletion</h4>
-        <p>Are you sure you want to delete this book?</p>
-        <button onClick={onDelete}>Yes, delete</button>
-        <button onClick={onClose}>Cancel</button>
+    <div className="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="exampleModalLabel">Delete Book {currentBook?.name}</h1>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"> X </button>
+          </div>
+          
+          <div className="modal-body">
+          <h4>Confirm Deletion</h4>
+          <p>Are you sure you want to delete this book?</p>
+          </div>
+            
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-primary" onClick={() => deleteBook()}>Delete</button>
+          </div>
+        </div>
       </div>
     </div>
+
   );
 };
 
--- Node/react-app/src/pages/Dashboard.jsx
+++ Node/react-app/src/pages/Dashboard.jsx
@@ -14,22 +14,9 @@ const Dashboard = () => {
     console.log(books);
   },[]);
 
-  const handleEdit = (book) => {
+  const setBook = (book) => {
     setCurrentBook(book);
-    setName(currentBook.name);
-    setAuthor(currentBook.author);
-  };
-
-  const deleteBook = () => {
-    axios.delete(`http://localhost:8080/api/books/${currentBook._id}`)
-    .then((res) => { 
-      console.log(res.data); 
-      books.filter((el) => el._id !== currentBook._id );
-      setBooks(books);
-      alert(res.data.message);
-    } )
-    .catch( (err)=> console.log(err))
-  };
+  }
 
   return (
     <div className='mt-5'>
@@ -42,14 +29,15 @@ const Dashboard = () => {
             <th>Actions</th>
           </tr>
         </thead>
+
         <tbody>
           {books.map((book) => (
             <tr key={book._id}>
               <td className='mx-5' >{book.author}</td>
               <td className='mx-5'>{book.name}</td>
               <td>
-                <button className='mx-3 btn btn-info' onClick={() => handleEdit(book)} data-bs-toggle='modal' data-bs-target="#exampleModal">Edit</button>
-                <button className='mx-3 btn btn-danger' onClick={() => handleDelete(book)} data-bs-toggle='modal' data-bs-target="#deleteModal">Delete</button>
+                <button className='mx-3 btn btn-info' data-bs-toggle='modal' onClick={()=> setBook(book)} data-bs-target="#exampleModal">Edit</button>
+                <button className='mx-3 btn btn-danger' data-bs-toggle='modal' onClick={()=> setBook(book)} data-bs-target="#deleteModal">Delete</button>
               </td>
             </tr>
           ))}
@@ -57,29 +45,7 @@ const Dashboard = () => {
       </table>
 
       <UpdateModal currentBook={currentBook} />
-
-    {/* deleteModal */}
-    <div className="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h1 className="modal-title fs-5" id="exampleModalLabel">Delete Book {currentBook?.name}</h1>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"> X </button>
-          </div>
-          
-          <div className="modal-body">
-          <h4>Confirm Deletion</h4>
-          <p>Are you sure you want to delete this book?</p>
-          </div>
-            
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" className="btn btn-primary" onClick={() => deleteBook()}>Delete</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
+      <DeleteModal currentBook={currentBook}/>
     </div>
   );
 };
