9bdda11ab40b3ccbea55df50003b404f8a70db68
Omar Sayed <omarkhalil117@gmail.com>
2024-08-26 17:45:30 +0300

    practice async thunk methods

--- Node/react-app/src/components/FirstComponent.jsx
+++ Node/react-app/src/components/FirstComponent.jsx
@@ -1,15 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "../store/slices/counterSlice";
+import { getDataFromApi } from "../store/slices/counterSlice";
+
 function FirstComponent() {
     
-    const count = useSelector(state => state.counter.value) 
+    const { count , apiData , isLoading } = useSelector(state => state.counter) 
+
     const dispatch = useDispatch();
-    
+
    return (
     <>
         <h1 className='text-center'>Count is {count} in Comp (1)</h1>
         <button className="btn btn-primary" onClick={() => dispatch(increment())}> + </button> 
         <button className="btn btn-danger" onClick={() => dispatch(decrement())}> - </button> 
+
+        {isLoading ? <h1>Loading...</h1> : <h5>Data : {apiData || "not assigned"}</h5>}
+
+        <button className="btn btn-primary" onClick={() => dispatch(getDataFromApi())} >get data</button>
     </>
   )
 }
--- Node/react-app/src/store/slices/counterSlice.js
+++ Node/react-app/src/store/slices/counterSlice.js
@@ -1,8 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   value: 0,
+  apiData: '',
+  isLoading: false
 };
+
+export const getDataFromApi = createAsyncThunk( 
+    "counter/getDataFromApi",
+    async () => {
+        const res = await axios.get('http://localhost:8080/api/dummy');
+        return res.data;
+    }
+ )
 
 const counterSlice = createSlice({
   name: 'counter',
@@ -18,6 +29,21 @@ const counterSlice = createSlice({
       state.value += action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder
+    .addCase(getDataFromApi.pending, (state) => {
+        state.isLoading = true;
+    })
+    .addCase(getDataFromApi.fulfilled, (state, action) => {
+        state.apiData = action.payload.data
+        state.isLoading = false;
+    })
+    .addCase(getDataFromApi.rejected, (state, action) => {
+        state.apiData = action.error.message;
+        state.isLoading = false;
+    })
+ 
+  }
 });
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
